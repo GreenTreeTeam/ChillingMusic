fd4b7d74e6533f17072e268dd9a64899a8a9512e
Sudhan <sudhanplayz@gmail.com>
2021-06-06 13:44:43 +0000

    fix: removed old files, added color

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 //Components
 import Header from "./components/header"
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
-import './index.css';
+import './styles/index.sass';
 import App from './App';
 
 ReactDOM.render(
